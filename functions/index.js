40336bd6d20602e965a69f8271a0bc2a6ddbdcd7
Fomagran <fomagran6@naver.com>
2021-05-11 11:51:26 +0900

    토큰 디플로이 성공!! functions 안에 명령어를 쳐야했는데 AgoraRtc 폴더에 치고 있었음...

--- functions/index.js
+++ functions/index.js
@@ -1,42 +1,28 @@
-const express = require('express');
 const functions = require("firebase-functions").region("asia-northeast3");
-const admin = require("firebase-admin");
-
 const {RtcTokenBuilder, RtcRole} = require("agora-access-token");
 const APP_ID = "f6b5501a22f64b6b9e26ac9332d93d9f";
 const APP_CERTIFICATE = "07ff45c4d5094a95b58a88531548dd81";
-const app = express();
 
+exports.generateAccessToken = functions.https.onCall((data) => {
 
-admin.initializeApp();
-
-const nocache = (req, resp, next) => {
-  resp.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-  resp.header('Expires', '-1');
-  resp.header('Pragma', 'no-cache');
-  next();
-}
-
-exports.generateAccessToken = functions.https.onRequest((req, res) => {
-  // set response header
-  resp.header('Acess-Control-Allow-Origin', '*');
   // get channel name
   const channelName = req.query.channelName;
+  
   if (!channelName) {
-    return resp.status(500).json({ 'error': 'channel is required' });
+    return data.status(500).json({ 'error': 'channel is required' });
   }
   // get uid 
-  let uid = req.query.uid;
+  let uid = data.uid;
   if(!uid || uid == '') {
     uid = 0;
   }
   // get role
   let role = RtcRole.SUBSCRIBER;
-  if (req.query.role == 'publisher') {
+  if (data.role == 'publisher') {
     role = RtcRole.PUBLISHER;
   }
   // get the expire time
-  let expireTime = req.query.expireTime;
+  let expireTime = data.expireTime;
   if (!expireTime || expireTime == '') {
     expireTime = 3600;
   } else {
@@ -48,12 +34,5 @@ exports.generateAccessToken = functions.https.onRequest((req, res) => {
   // build the token
   const token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, channelName, uid, role, privilegeExpireTime);
   // return the token
-  return resp.json({ 'token': token });
+  return data.json({ 'token': token });
 });
-
-const cors = require('cors');
-app.use(cors({ origin: true }));
-app.use(myMiddleware);
-
-app.get('/:id', (req, res) => res.send(Widgets.getById(req.params.id)));
-exports.widgets = functions.https.onRequest(app);
