9da39d499e214aeecc1833bd4c28932452e3bed6
Fomagran <fomagran6@naver.com>
2021-05-11 17:23:32 +0900

    subscriber publisher에 따라서 바꾸기

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,37 @@
+//firebase funtions 한국 서버로 설정
 const functions = require("firebase-functions").region("asia-northeast3");
+//Agora 액세스 토큰빌더,역할 설정
 const {RtcTokenBuilder, RtcRole} = require("agora-access-token");
-const APP_ID = "f6b5501a22f64b6b9e26ac9332d93d9f";
-const APP_CERTIFICATE = "07ff45c4d5094a95b58a88531548dd81";
+//Agora 앱아이디
+const APP_ID = "1186f40c5f4743a7b0650c9a6a0565d6";
+//Agora 시크릿
+const APP_CERTIFICATE = "ac57e6cc2fb144c1a8659423f1d7724e";
 
-exports.generateAccessToken = functions.https.onCall((data) => {
+//Cloud Functions으로 토큰 생성 함수 만듦.
+exports.generateToken = functions.https.onCall((data)=>{
 
-  // get channel name
-  const channelName = req.query.channelName;
-  
-  if (!channelName) {
-    return data.status(500).json({ 'error': 'channel is required' });
-  }
-  // get uid 
-  let uid = data.uid;
-  if(!uid || uid == '') {
-    uid = 0;
-  }
-  // get role
+  //Agora 채널이름
+  const channelName = data.channelName;
+  //Agora uid
+  const uid = data.uid;
+  //Agora 역할
   let role = RtcRole.SUBSCRIBER;
+
   if (data.role == 'publisher') {
     role = RtcRole.PUBLISHER;
   }
-  // get the expire time
-  let expireTime = data.expireTime;
-  if (!expireTime || expireTime == '') {
-    expireTime = 3600;
-  } else {
-    expireTime = parseInt(expireTime, 10);
-  }
-  // calculate privilege expire time
+
+  //중단되는 시간
+  const expireTime = data.expireTime;
+
+  //현재시간
   const currentTime = Math.floor(Date.now() / 1000);
+  //현재부터 중단되는 시간
   const privilegeExpireTime = currentTime + expireTime;
-  // build the token
-  const token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, channelName, uid, role, privilegeExpireTime);
-  // return the token
-  return data.json({ 'token': token });
+  //Agora 토큰
+  const token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE,
+      channelName, uid, role, privilegeExpireTime);
+
+  //json형식으로 token값 반환
+  return {"token" : token};
 });
